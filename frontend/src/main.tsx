c6b897eea8f6d70f5c47c38f9fdccf79a7e2eabf
TheShad0wNinja <blacklistmillion@gmail.com>
2024-01-07 17:24:33 +0200

    build: Added required packages for frontend

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,28 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './pages/ErrorPage';
+import Root from './pages/Root';
+import Home from './pages/Home';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Root />,
+		errorElement: <ErrorPage />,
+		children: [
+			{
+				path: '/', 
+				element: <Home />
+			},
+		]
+	},
+	
+])
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+		<RouterProvider router={router}/>
   </React.StrictMode>,
 )
